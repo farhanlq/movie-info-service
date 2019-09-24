a55cd215913d189bf1523f619be058b52598c736
Farhan Laeeq <far619@gmail.com>
2019-09-24 14:06:05 +0530

    Added the code for calling external api

--- src/main/java/com/example/microservices/movieinfoservice/model/Movie.java
+++ src/main/java/com/example/microservices/movieinfoservice/model/Movie.java
@@ -1,16 +1,17 @@
 package com.example.microservices.movieinfoservice.model;
 
 public class Movie {
-
 	private String movieId;
 	private String name;
+	private String description;
 
 	public Movie() {
 	}
 
-	public Movie(String movieId, String name) {
+	public Movie(String movieId, String name, String description) {
 		this.movieId = movieId;
 		this.name = name;
+		this.description = description;
 	}
 
 	public String getMovieId() {
@@ -27,6 +28,14 @@ public class Movie {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 }
